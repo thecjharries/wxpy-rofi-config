77694e6dc7bbbc6bd27e3c5804cde01b808d5f0c
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-17 08:24:48 -0600

    Ignore warnings about forced wxPython naming

--- wxpy_rofi_config/gui/config_app.py
+++ wxpy_rofi_config/gui/config_app.py
@@ -1,25 +1,27 @@
 """This file provides the ConfigApp class and cli access"""
-# pylint: disable=W,C,R
 
-# pylint: disable=no-name-in-module
+
 from wx import (
     App
 )
-# pylint: enable=no-name-in-module
+
 
 # from wxpy_rofi_config.config import Rofi
 from wxpy_rofi_config.gui import ConfigFrame
 
 
 class ConfigApp(App):
+    """ConfigApp provides the main GUI"""
 
-    def OnInit(self):
+    def OnInit(self):  # pylint:disable=invalid-name
+        """Initializes the GUI"""
         frame = ConfigFrame()
         frame.Show()
         return True
 
 
 def cli():
+    """Checks if the module has been loaded via the CLI"""
     if '__main__' == __name__:
         app = ConfigApp(False)
         app.MainLoop()
