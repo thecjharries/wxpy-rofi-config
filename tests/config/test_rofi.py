5ea9e43dfaffa5d83388b4454e1aad9609c00eb7
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-17 02:07:48 -0600

    Create base test case

---
+++ tests/config/test_rofi.py
@@ -0,0 +1,25 @@
+# pylint: disable=attribute-defined-outside-init
+# pylint: disable=missing-docstring
+# pylint: disable=unused-argument
+
+from __future__ import print_function
+
+from unittest import TestCase
+
+from mock import call, MagicMock, patch
+from pytest import mark
+
+from wxpy_rofi_config.config import Rofi
+
+
+class RofiTestCase(TestCase):
+
+    def setUp(self):
+        self.construct_rofi()
+        self.addCleanup(self.wipe_rofi)
+
+    def wipe_rofi(self):
+        del self.rofi
+
+    def construct_rofi(self):
+        self.rofi = Rofi()
