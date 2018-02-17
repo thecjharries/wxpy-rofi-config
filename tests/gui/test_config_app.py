be8f1d500dc563c4c6daa928c71fa16cc9365c78
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-17 08:49:35 -0600

    Refactor tests to match new init strategy

--- tests/gui/test_config_app.py
+++ tests/gui/test_config_app.py
@@ -5,12 +5,12 @@ from __future__ import print_function
 from mock import MagicMock, patch
 
 
-@patch('wxpy_rofi_config.gui.config_app.App')
-@patch('wxpy_rofi_config.gui.config_app.ConfigFrame')
-def test_on_init(mock_frame, mock_app):  # pylint: disable=unused-argument
-    from wxpy_rofi_config import ConfigApp
-    ConfigApp()
-    mock_frame.assert_called_once_with()
+def test_on_init():
+    from wxpy_rofi_config import config_app  # pylint: disable=no-name-in-module
+    with patch('wxpy_rofi_config.gui.config_app.App', MagicMock()):
+        with patch('wxpy_rofi_config.gui.config_app.ConfigFrame') as mock_frame:
+            config_app.ConfigApp()
+            mock_frame.assert_called_once_with()
 
 
 def test_cli():
@@ -18,4 +18,4 @@ def test_cli():
     with patch.object(config_app, "ConfigApp", return_value=MagicMock()) as mock_app:
         with patch.object(config_app, "__name__", "__main__"):
             config_app.cli()
-            mock_app.assert_called_once_with(False)
+            mock_app.assert_called_once_with()
