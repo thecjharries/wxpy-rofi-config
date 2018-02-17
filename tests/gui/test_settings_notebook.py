7c87abd763a788b26d47d92903193b832d154539
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-17 05:32:26 -0600

    Create SettingsNotebookTestCase

---
+++ tests/gui/test_settings_notebook.py
@@ -0,0 +1,52 @@
+# pylint: disable=attribute-defined-outside-init
+# pylint: disable=missing-docstring
+# pylint: disable=too-many-instance-attributes
+# pylint: disable=unused-argument
+
+from __future__ import print_function
+
+from unittest import TestCase
+
+from mock import MagicMock, patch
+
+from wxpy_rofi_config.gui import SettingsNotebook
+
+
+class SettingsNotebookTestCase(TestCase):
+
+    def setUp(self):
+        self.patch_wx()
+        self.construct_notebook()
+        self.addCleanup(self.wipe_notebook)
+
+    def wipe_notebook(self):
+        del self.notebook
+
+    def patch_wx(self):
+        findwindowbyname_patcher = patch(
+            'wxpy_rofi_config.gui.settings_notebook.FindWindowByName'
+        )
+        self.mock_findwindowbyname = findwindowbyname_patcher.start()
+        self.addCleanup(findwindowbyname_patcher.stop)
+        notebook_patcher = patch(
+            'wxpy_rofi_config.gui.settings_notebook.Notebook'
+        )
+        self.mock_notebook = notebook_patcher.start()
+        self.addCleanup(notebook_patcher.stop)
+
+    def construct_notebook(self):
+        settings_panel_patcher = patch(
+            'wxpy_rofi_config.gui.settings_notebook.SettingsPanel'
+        )
+        self.mock_settings_panel = settings_panel_patcher.start()
+        self.addCleanup(settings_panel_patcher.stop)
+        rofi_patcher = patch('wxpy_rofi_config.gui.settings_notebook.Rofi')
+        self.mock_rofi = rofi_patcher.start()
+        self.addCleanup(rofi_patcher.stop)
+        create_tabs_patcher = patch.object(SettingsNotebook, 'create_tabs')
+        self.mock_create_tabs = create_tabs_patcher.start()
+        bind_events_patcher = patch.object(SettingsNotebook, 'bind_events')
+        self.mock_bind_events = bind_events_patcher.start()
+        self.notebook = SettingsNotebook(None)
+        create_tabs_patcher.stop()
+        bind_events_patcher.stop()
