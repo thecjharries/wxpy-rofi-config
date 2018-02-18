1f96474dec8b59025ef1bfc8eb730584922c10a8
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 13:57:23 -0600

    Remove old files that will be replaced

--- wxpy_rofi_config/gui/__init__.py
+++ wxpy_rofi_config/gui/__init__.py
@@ -1,9 +1,3 @@
 """This file provides the gui submodule"""
 
 # pylint:disable=missing-docstring
-
-from .fitted_static_text import FittedStaticText
-from .settings_panel import SettingsPanel
-from .settings_notebook import SettingsNotebook
-from .config_frame import ConfigFrame
-from .config_app import ConfigApp
