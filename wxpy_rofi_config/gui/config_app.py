78de9ff738bda658cf2c29d705ce8cb345a827bf
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 18:41:35 -0600

    Add missing docstrings

--- wxpy_rofi_config/gui/config_app.py
+++ wxpy_rofi_config/gui/config_app.py
@@ -1,5 +1,7 @@
 # coding=utf8
 
+"""This file provides ConfigApp"""
+
 import wx
 import wx.lib.mixins.inspection
 
@@ -7,8 +9,13 @@ from wxpy_rofi_config.gui import ConfigFrame
 
 
 class ConfigApp(wx.App, wx.lib.mixins.inspection.InspectionMixin):
+    """This class runs the main application"""
 
     def OnInit(self):
+        """
+        The OnInit is used instead of __init__ to properly handle the wxPython
+        boot and InspectionMixin
+        """
         self.Init()  # initialize the inspection tool
         frame = ConfigFrame(None, title="rofi Configuration")
         frame.Show()
