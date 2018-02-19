c3125830414d65dc0f56e5b90efabc01718bb8d6
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 21:20:30 -0600

    Split out GUI construction

--- wxpy_rofi_config/gui/config_page.py
+++ wxpy_rofi_config/gui/config_page.py
@@ -27,29 +27,15 @@ from wxpy_rofi_config.gui import HidableAutoWrapStaticText
 class ConfigPage(Panel):
     """ConfigPage holds the GUI for a single tab pane"""
 
+    config = None
+    grid_sizer = None
+    main_sizer = None
+    scrolled_panel = None
+
     def __init__(self, parent, config):
         Panel.__init__(self, parent=parent, id=ID_ANY)
-
-        self.scrolled_panel = ScrolledPanel(self)
-        self.scrolled_panel.SetAutoLayout(1)
-        self.scrolled_panel.SetupScrolling()
-        self.main_sizer = BoxSizer(HORIZONTAL)
-        self.grid_sizer = FlexGridSizer(2, 10, 10)
-        for index, entry in enumerate(config):
-            if index > 0:
-                self.construct_horizontal_rule()
-            if entry.help_value:
-                self.construct_docs_label('help_value', entry.help_value)
-            self.construct_entry_label(entry.key_name)
-            self.construct_entry_label(entry)
-            if entry.man:
-                self.construct_docs_label('man', entry.man)
-        self.grid_sizer.AddGrowableCol(1, 1)
-        scroll_sizer = BoxSizer(HORIZONTAL)
-        scroll_sizer.Add(self.grid_sizer, 1, EXPAND | ALL, 10)
-        self.scrolled_panel.SetSizer(scroll_sizer)
-        self.main_sizer.Add(self.scrolled_panel, 1, EXPAND)
-        self.SetSizer(self.main_sizer)
+        self.config = config
+        self.construct_gui()
 
     def construct_horizontal_rule(self):
         """Adds a horizontal rule to the grid"""
@@ -99,3 +85,30 @@ class ConfigPage(Panel):
             size=(-1, -1)
         )
         self.grid_sizer.Add(current_value, -1, EXPAND)
+
+    def construct_entry_rows(self, entry, index=0):
+        """Constructs all the necessary rows for a single entry"""
+        if index > 0:
+            self.construct_horizontal_rule()
+        if entry.help_value:
+            self.construct_docs_label('help_value', entry.help_value)
+        self.construct_entry_label(entry.key_name)
+        self.construct_entry_label(entry)
+        if entry.man:
+            self.construct_docs_label('man', entry.man)
+
+    def construct_gui(self):
+        """Constructs the page GUI"""
+        self.scrolled_panel = ScrolledPanel(self)
+        self.scrolled_panel.SetAutoLayout(1)
+        self.scrolled_panel.SetupScrolling()
+        self.main_sizer = BoxSizer(HORIZONTAL)
+        self.grid_sizer = FlexGridSizer(2, 10, 10)
+        for index, entry in enumerate(self.config):
+            self.construct_entry_rows(entry, index)
+        self.grid_sizer.AddGrowableCol(1, 1)
+        scroll_sizer = BoxSizer(HORIZONTAL)
+        scroll_sizer.Add(self.grid_sizer, 1, EXPAND | ALL, 10)
+        self.scrolled_panel.SetSizer(scroll_sizer)
+        self.main_sizer.Add(self.scrolled_panel, 1, EXPAND)
+        self.SetSizer(self.main_sizer)
