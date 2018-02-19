77cd724927d636ea00a03817c5a118f5446ddacd
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 19:00:12 -0600

    Add missing docstrings

--- wxpy_rofi_config/gui/hidable_auto_wrap_static_test.py
+++ wxpy_rofi_config/gui/hidable_auto_wrap_static_test.py
@@ -1,5 +1,7 @@
 # coding=utf8
 
+"""This file provide HidableAutoWrapStaticText"""
+
 import wx
 from wx.lib.pubsub import pub
 from wx.lib.agw.infobar import AutoWrapStaticText
@@ -8,12 +10,17 @@ from wxpy_rofi_config.config import Rofi
 
 
 class HidableAutoWrapStaticText(AutoWrapStaticText):
+    """
+    HidableAutoWrapStaticText adds a toggle handler and listens for hide/show
+    actions. Its parent automatically resizes StaticText controls.
+    """
 
     def __init__(self, parent=None, label="", kind='help_value'):
         AutoWrapStaticText.__init__(self, parent, label)
         pub.subscribe(self.toggle_display, "toggle_display_%s" % kind)
 
     def toggle_display(self, data):
+        """Shows or hides the control based on the published message"""
         if data:
             action = 'Show'
         else:
