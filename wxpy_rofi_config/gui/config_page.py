c71ba53795252a70b6f33ca643d800ae9672963a
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 18:54:53 -0600

    Tidy imports

--- wxpy_rofi_config/gui/config_page.py
+++ wxpy_rofi_config/gui/config_page.py
@@ -2,73 +2,88 @@
 
 """This file provides ConfigPage"""
 
-import wx
+from wx import (
+    ALIGN_RIGHT,
+    ALL,
+    BOTTOM,
+    BoxSizer,
+    EXPAND,
+    FlexGridSizer,
+    HORIZONTAL,
+    ID_ANY,
+    LI_HORIZONTAL,
+    Panel,
+    StaticLine,
+    StaticText,
+    TextCtrl,
+    TOP,
+    VERTICAL,
+)
 from wx.lib.scrolledpanel import ScrolledPanel
 
-from wxpy_rofi_config.config import Rofi
 from wxpy_rofi_config.gui import HidableAutoWrapStaticText
 
 
-class ConfigPage(wx.Panel):
+class ConfigPage(Panel):
     """ConfigPage holds the GUI for a single tab pane"""
 
     def __init__(self, parent, config):
-        wx.Panel.__init__(self, parent=parent, id=wx.ID_ANY)
+        Panel.__init__(self, parent=parent, id=ID_ANY)
 
         scrolled_panel = ScrolledPanel(self)
         scrolled_panel.SetAutoLayout(1)
         scrolled_panel.SetupScrolling()
-        main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        grid_sizer = wx.FlexGridSizer(2, 10, 10)
+        main_sizer = BoxSizer(HORIZONTAL)
+        grid_sizer = FlexGridSizer(2, 10, 10)
         for index, entry in enumerate(config):
             if index > 0:
                 for _ in range(0, 2):
-                    rule = wx.StaticLine(
+                    rule = StaticLine(
                         scrolled_panel,
-                        style=wx.LI_HORIZONTAL,
+                        style=LI_HORIZONTAL,
                         size=(-1, 2)
                     )
-                    grid_sizer.Add(rule, proportion=1, flag=wx.EXPAND |
-                                   wx.TOP | wx.BOTTOM, border=10)
+                    grid_sizer.Add(rule, proportion=1, flag=EXPAND |
+                                   TOP | BOTTOM, border=10)
             if entry.help_value:
-                grid_sizer.Add(1, 0, 1, wx.EXPAND)
-                help_sizer = wx.BoxSizer(wx.HORIZONTAL)
+                grid_sizer.Add(1, 0, 1, EXPAND)
+                help_sizer = BoxSizer(HORIZONTAL)
                 help_label = HidableAutoWrapStaticText(
                     parent=scrolled_panel,
                     label=u"%s" % entry.help_value.decode('utf8', 'ignore'),
                     kind='help_value',
                 )
-                help_sizer.Add(help_label, -1, wx.EXPAND)
-                grid_sizer.Add(help_sizer, 1, wx.EXPAND)
-            label_sizer = wx.BoxSizer(wx.VERTICAL)
-            label_sizer.Add(0, 1, 1, wx.EXPAND)
-            label = wx.StaticText(
+                help_sizer.Add(help_label, -1, EXPAND)
+                grid_sizer.Add(help_sizer, 1, EXPAND)
+            label_sizer = BoxSizer(VERTICAL)
+            label_sizer.Add(0, 1, 1, EXPAND)
+            label = StaticText(
                 scrolled_panel,
                 label=entry.key_name,
-                style=wx.ALIGN_RIGHT
+                style=ALIGN_RIGHT
             )
-            label_sizer.Add(label, flag=wx.ALIGN_RIGHT)
-            label_sizer.Add(0, 1, 1, wx.EXPAND)
-            grid_sizer.Add(label_sizer, 0, wx.EXPAND)
-            current_value = wx.TextCtrl(
+            label_sizer.Add(label, flag=ALIGN_RIGHT)
+            label_sizer.Add(0, 1, 1, EXPAND)
+            grid_sizer.Add(label_sizer, 0, EXPAND)
+            current_value = TextCtrl(
                 scrolled_panel,
                 value=str(entry.current),
                 size=(-1, -1)
             )
-            grid_sizer.Add(current_value, -1, wx.EXPAND)
+            grid_sizer.Add(current_value, -1, EXPAND)
             if entry.man:
-                grid_sizer.Add(1, 0, 1, wx.EXPAND)
-                man_sizer = wx.BoxSizer(wx.HORIZONTAL)
+                grid_sizer.Add(1, 0, 1, EXPAND)
+                man_sizer = BoxSizer(HORIZONTAL)
                 man_label = HidableAutoWrapStaticText(
                     parent=scrolled_panel,
                     label=u"%s" % entry.man.decode('utf8', 'ignore'),
                     kind='man',
                 )
-                man_sizer.Add(man_label, -1, wx.EXPAND)
-                grid_sizer.Add(man_sizer, 1, wx.EXPAND)
+                man_sizer.Add(man_label, -1, EXPAND)
+                grid_sizer.Add(man_sizer, 1, EXPAND)
         grid_sizer.AddGrowableCol(1, 1)
-        scroll_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        scroll_sizer.Add(grid_sizer, 1, wx.EXPAND | wx.ALL, 10)
+        scroll_sizer = BoxSizer(HORIZONTAL)
+        scroll_sizer.Add(grid_sizer, 1, EXPAND | ALL, 10)
         scrolled_panel.SetSizer(scroll_sizer)
-        main_sizer.Add(scrolled_panel, 1, wx.EXPAND)
+        main_sizer.Add(scrolled_panel, 1, EXPAND)
         self.SetSizer(main_sizer)
