1de04fa30da1657b303931ee276d2fc6068dda5b
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 21:14:37 -0600

    Split out doc label creation (help_value, man)

--- wxpy_rofi_config/gui/config_page.py
+++ wxpy_rofi_config/gui/config_page.py
@@ -39,15 +39,7 @@ class ConfigPage(Panel):
             if index > 0:
                 self.construct_horizontal_rule()
             if entry.help_value:
-                self.grid_sizer.Add(1, 0, 1, EXPAND)
-                help_sizer = BoxSizer(HORIZONTAL)
-                help_label = HidableAutoWrapStaticText(
-                    parent=self.scrolled_panel,
-                    label=u"%s" % entry.help_value.decode('utf8', 'ignore'),
-                    kind='help_value',
-                )
-                help_sizer.Add(help_label, -1, EXPAND)
-                self.grid_sizer.Add(help_sizer, 1, EXPAND)
+                self.construct_docs_label('help_value', entry.help_value)
             label_sizer = BoxSizer(VERTICAL)
             label_sizer.Add(0, 1, 1, EXPAND)
             label = StaticText(
@@ -65,15 +57,7 @@ class ConfigPage(Panel):
             )
             self.grid_sizer.Add(current_value, -1, EXPAND)
             if entry.man:
-                self.grid_sizer.Add(1, 0, 1, EXPAND)
-                man_sizer = BoxSizer(HORIZONTAL)
-                man_label = HidableAutoWrapStaticText(
-                    parent=self.scrolled_panel,
-                    label=u"%s" % entry.man.decode('utf8', 'ignore'),
-                    kind='man',
-                )
-                man_sizer.Add(man_label, -1, EXPAND)
-                self.grid_sizer.Add(man_sizer, 1, EXPAND)
+                self.construct_docs_label('man', entry.man)
         self.grid_sizer.AddGrowableCol(1, 1)
         scroll_sizer = BoxSizer(HORIZONTAL)
         scroll_sizer.Add(self.grid_sizer, 1, EXPAND | ALL, 10)
@@ -95,3 +79,15 @@ class ConfigPage(Panel):
                 flag=EXPAND | TOP | BOTTOM,
                 border=10
             )
+
+    def construct_docs_label(self, kind, value):
+        """Constructs a documentation label"""
+        self.grid_sizer.Add(1, 0, 1, EXPAND)
+        sizer = BoxSizer(HORIZONTAL)
+        label = HidableAutoWrapStaticText(
+            parent=self.scrolled_panel,
+            label=u"%s" % value.decode('utf8', 'ignore'),
+            kind=kind,
+        )
+        sizer.Add(label, -1, EXPAND)
+        self.grid_sizer.Add(sizer, 1, EXPAND)
