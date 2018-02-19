40ba2e0172a541018609e6f11c3df3d68d603edc
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 21:03:52 -0600

    Export ConfigFrameStatusBar

--- wxpy_rofi_config/gui/__init__.py
+++ wxpy_rofi_config/gui/__init__.py
@@ -5,5 +5,6 @@
 from .hidable_auto_wrap_static_test import HidableAutoWrapStaticText
 from .config_page import ConfigPage
 from .config_frame_menu_bar import ConfigFrameMenuBar
+from .config_frame_status_bar import ConfigFrameStatusBar
 from .config_frame import ConfigFrame
 from .config_app import ConfigApp
