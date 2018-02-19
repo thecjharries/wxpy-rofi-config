0de1cb54bdee6c9ca171c1a529d5019b038b5fd7
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 21:11:03 -0600

    Make scrolled_panel an instance variable

--- wxpy_rofi_config/gui/config_page.py
+++ wxpy_rofi_config/gui/config_page.py
@@ -30,16 +30,16 @@ class ConfigPage(Panel):
     def __init__(self, parent, config):
         Panel.__init__(self, parent=parent, id=ID_ANY)
 
-        scrolled_panel = ScrolledPanel(self)
-        scrolled_panel.SetAutoLayout(1)
-        scrolled_panel.SetupScrolling()
+        self.scrolled_panel = ScrolledPanel(self)
+        self.scrolled_panel.SetAutoLayout(1)
+        self.scrolled_panel.SetupScrolling()
         self.main_sizer = BoxSizer(HORIZONTAL)
         self.grid_sizer = FlexGridSizer(2, 10, 10)
         for index, entry in enumerate(config):
             if index > 0:
                 for _ in range(0, 2):
                     rule = StaticLine(
-                        scrolled_panel,
+                        self.scrolled_panel,
                         style=LI_HORIZONTAL,
                         size=(-1, 2)
                     )
@@ -49,7 +49,7 @@ class ConfigPage(Panel):
                 self.grid_sizer.Add(1, 0, 1, EXPAND)
                 help_sizer = BoxSizer(HORIZONTAL)
                 help_label = HidableAutoWrapStaticText(
-                    parent=scrolled_panel,
+                    parent=self.scrolled_panel,
                     label=u"%s" % entry.help_value.decode('utf8', 'ignore'),
                     kind='help_value',
                 )
@@ -58,7 +58,7 @@ class ConfigPage(Panel):
             label_sizer = BoxSizer(VERTICAL)
             label_sizer.Add(0, 1, 1, EXPAND)
             label = StaticText(
-                scrolled_panel,
+                self.scrolled_panel,
                 label=entry.key_name,
                 style=ALIGN_RIGHT
             )
@@ -66,7 +66,7 @@ class ConfigPage(Panel):
             label_sizer.Add(0, 1, 1, EXPAND)
             self.grid_sizer.Add(label_sizer, 0, EXPAND)
             current_value = TextCtrl(
-                scrolled_panel,
+                self.scrolled_panel,
                 value=str(entry.current),
                 size=(-1, -1)
             )
@@ -75,7 +75,7 @@ class ConfigPage(Panel):
                 self.grid_sizer.Add(1, 0, 1, EXPAND)
                 man_sizer = BoxSizer(HORIZONTAL)
                 man_label = HidableAutoWrapStaticText(
-                    parent=scrolled_panel,
+                    parent=self.scrolled_panel,
                     label=u"%s" % entry.man.decode('utf8', 'ignore'),
                     kind='man',
                 )
@@ -84,6 +84,6 @@ class ConfigPage(Panel):
         self.grid_sizer.AddGrowableCol(1, 1)
         scroll_sizer = BoxSizer(HORIZONTAL)
         scroll_sizer.Add(self.grid_sizer, 1, EXPAND | ALL, 10)
-        scrolled_panel.SetSizer(scroll_sizer)
-        self.main_sizer.Add(scrolled_panel, 1, EXPAND)
+        self.scrolled_panel.SetSizer(scroll_sizer)
+        self.main_sizer.Add(self.scrolled_panel, 1, EXPAND)
         self.SetSizer(self.main_sizer)
