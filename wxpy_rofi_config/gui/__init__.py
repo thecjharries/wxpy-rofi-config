df71ed6f2d61e54ef7e87ff4014f2c879505239b
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-18 19:14:25 -0600

    Import ConfigFrameMenuBar

--- wxpy_rofi_config/gui/__init__.py
+++ wxpy_rofi_config/gui/__init__.py
@@ -4,5 +4,6 @@
 
 from .hidable_auto_wrap_static_test import HidableAutoWrapStaticText
 from .config_page import ConfigPage
+from .config_frame_menu_bar import ConfigFrameMenuBar
 from .config_frame import ConfigFrame
 from .config_app import ConfigApp
