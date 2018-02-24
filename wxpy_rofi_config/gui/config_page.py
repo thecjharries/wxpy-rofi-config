8344bca0d2100f779aba350802a29a8ba8a39931
CJ Harries <cj@wizardsoftheweb.pro>
2018-02-24 12:03:43 -0600

    Refactor layout to single column with large font

--- wxpy_rofi_config/gui/config_page.py
+++ wxpy_rofi_config/gui/config_page.py
@@ -3,13 +3,15 @@
 """This file provides ConfigPage"""
 
 from wx import (
-    ALIGN_RIGHT,
+    ALIGN_LEFT,
     ALL,
     BOTTOM,
     BoxSizer,
     CheckBox,
     EXPAND,
     FlexGridSizer,
+    Font,
+    FontInfo,
     HORIZONTAL,
     ID_ANY,
     LI_HORIZONTAL,
@@ -37,47 +39,41 @@ class ConfigPage(Panel):
     def __init__(self, parent, config):
         Panel.__init__(self, parent=parent, id=ID_ANY)
         self.config = config
+        self.header_font = Font(FontInfo(24).Bold())
         self.construct_gui()
 
     def construct_horizontal_rule(self):
         """Adds a horizontal rule to the grid"""
-        for _ in range(0, 2):
-            rule = StaticLine(
-                self.scrolled_panel,
-                style=LI_HORIZONTAL,
-                size=(-1, 2)
-            )
-            self.grid_sizer.Add(
-                rule,
-                proportion=1,
-                flag=EXPAND | TOP | BOTTOM,
-                border=10
-            )
+        rule = StaticLine(
+            self.scrolled_panel,
+            style=LI_HORIZONTAL,
+            size=(-1, 2)
+        )
+        self.grid_sizer.Add(
+            rule,
+            proportion=1,
+            flag=EXPAND | TOP | BOTTOM,
+            border=10
+        )
 
     def construct_docs_label(self, kind, value):
         """Constructs a documentation label"""
-        self.grid_sizer.Add(1, -1, 1, EXPAND)
-        sizer = BoxSizer(HORIZONTAL)
         label = HidableAutoWrapStaticText(
             parent=self.scrolled_panel,
             label=value,
             kind=kind,
         )
-        sizer.Add(label, -1, EXPAND)
-        self.grid_sizer.Add(sizer, -1, EXPAND)
+        self.grid_sizer.Add(label, -1, EXPAND)
 
     def construct_entry_label(self, value):
         """Creates the primary entry label"""
-        label_sizer = BoxSizer(VERTICAL)
-        label_sizer.Add(0, 1, 1, EXPAND)
         label = StaticText(
             self.scrolled_panel,
             label=value,
-            style=ALIGN_RIGHT
+            style=ALIGN_LEFT
         )
-        label_sizer.Add(label, flag=ALIGN_RIGHT)
-        label_sizer.Add(0, 1, 1, EXPAND)
-        self.grid_sizer.Add(label_sizer, 0, EXPAND)
+        label.SetFont(self.header_font)
+        self.grid_sizer.Add(label, -1, EXPAND)
 
     def construct_entry_control(self, entry):
         """Creates the primary entry control"""
@@ -106,9 +102,9 @@ class ConfigPage(Panel):
         """Constructs all the necessary rows for a single entry"""
         if index > 0:
             self.construct_horizontal_rule()
+        self.construct_entry_label(entry.key_name)
         if entry.help_value:
             self.construct_docs_label('help_value', entry.help_value)
-        self.construct_entry_label(entry.key_name)
         self.construct_entry_control(entry)
         if entry.man:
             self.construct_docs_label('man', entry.man)
@@ -119,10 +115,10 @@ class ConfigPage(Panel):
         self.scrolled_panel.SetAutoLayout(1)
         self.scrolled_panel.SetupScrolling()
         self.main_sizer = BoxSizer(HORIZONTAL)
-        self.grid_sizer = FlexGridSizer(2, 10, 10)
+        self.grid_sizer = FlexGridSizer(1, 10, 10)
         for index, entry in enumerate(self.config):
             self.construct_entry_row(entry, index)
-        self.grid_sizer.AddGrowableCol(1, 1)
+        self.grid_sizer.AddGrowableCol(0, 1)
         scroll_sizer = BoxSizer(HORIZONTAL)
         scroll_sizer.Add(self.grid_sizer, 1, EXPAND | ALL, 10)
         self.scrolled_panel.SetSizer(scroll_sizer)
